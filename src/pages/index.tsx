d9a9ebfbee149e9d3a3b79481a7828b4abef4b46
Hwang Tae Hun <fjf332@naver.com>
2024-11-16 23:18:59 +0900

    fix URL

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,21 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import MainPage from './MainPage';
 import './MainPage.css';
 
 const Home: React.FC = () => {
+  useEffect(() => {
+    // URL 처리
+    if (window.location.hostname === 'ptutip.kro.kr') {
+      const newUrl = `https://www.${window.location.hostname}${window.location.pathname}${window.location.search}`;
+      window.location.replace(newUrl);
+    }
+
+    // 루트 경로 처리
+    if (window.location.pathname === '/') {
+      window.history.replaceState(null, '', '/MainPage');
+    }
+  }, []);
+
   return (
     <div>
       <MainPage />
